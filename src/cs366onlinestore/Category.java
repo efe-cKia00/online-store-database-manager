af44327824ede1b0fafeb53696cde5125e0ce9ea
KalanLarson <kalan.larson@ndus.edu>
2025-12-09 14:43:09 -0600

    Created Category Class

---
+++ src/cs366onlinestore/Category.java
@@ -0,0 +1,57 @@
+
+package cs366onlinestore;
+
+
+public class Category {
+    // ---------- Fields (from UML) ----------
+    private int categoryId;
+    private String name;
+    private String description;
+
+    // ---------- Constructors ----------
+    public Category() {
+        // no-arg constructor
+    }
+
+    public Category(int categoryId,
+                    String name,
+                    String description) {
+        this.categoryId = categoryId;
+        this.name = name;
+        this.description = description;
+    }
+
+    // ---------- Getters / Setters ----------
+    public int getCategoryId() {
+        return categoryId;
+    }
+
+    public void setCategoryId(int categoryId) {
+        this.categoryId = categoryId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "categoryId=" + categoryId +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
